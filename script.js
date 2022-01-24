c757832927ad0aebf089ca1f40802ed13644ab3e
DaveyPDev <74942934+DaveyPDev@users.noreply.github.com>
2022-01-23 21:41:07 -0500

    Add files via upload
    Uploading changes. Stuck on figuring out how to get an image to actually load.

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 // 3* //
-
 const form = document.querySelector('form');
-
-
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
     window.history.back()
     
+// 4* //
+    const submitText = document.querySelector('submit')
+    submitText.innerText = input.value;
+    input.value = '';
     
 }, true);
 
 
 form.addEventListener('click', function(e) {
     e.preventDefault()
-     // 4* //
-    // Looking to add submitted text //
-    const submitText = document.querySelector('submit')
-    submitText.innerText = input.value;
-    input.value = '';
-    // submit text //
+    
+   
     console.log(e)
 })
