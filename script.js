d48704287873ce046a7e0c6053878d3719a1488c
DaveyPDev <74942934+DaveyPDev@users.noreply.github.com>
2022-01-26 11:31:45 -0500

    Add files via upload
    Slowly making progress? 
    added to create div's

--- script.js
+++ script.js
@@ -4,13 +4,48 @@ const bodyClick = document.querySelector('body')
 
 memeForm.addEventListener ('submit', function(e) {
     e.preventDefault()
-   
-    
+//    start top text   //
     const topText = document.getElementById('top-text')
+    topText.innerText = input.value;
+    input.value = ''
     console.log(topText)
+//   end top text    //
+//    start bot text    //
     const botText = document.getElementById('bot-text')
+    botText.innerText = ''
     console.log(botText)
     console.log('Form submitted')
+//    end bot text   //
+//    start url submit
+    const memeUrl = document.createElement('img');
+    memeUrl.src = '';
+    newDiv.append(memeUrl)
+//    end url submit    //
+// 5*  //
+//   start top text div   //
+    const newDivImg = document.createElement('div')
+    const parentContainerImg = document.querySelector('.container')
+    parentContainerImg.append(newDivImg)
+    newDivImg.innerText = 'test img'; // make it url?
+    newDivImg.classList.add('meme-img')
+
+//    end top text div    //
+//   start top text div   //
+    const newDivTop = document.createElement('div')
+    const parentContainerTop = document.querySelector('.container')
+    parentContainerTop.append(newDivTop)
+    newDivTop.innerText = 'test top';
+    newDivTop.classList.add('top-text')
+
+//    end top text div    //
+//    start bot text div    //
+    const newDivBot = document.createElement('div')
+    const parentContainerBot = document.querySelector('.container')
+    parentContainerBot.append(newDivBot)
+    newDivBot.innerText = 'test bot';
+    newDivBot.classList.add('bot-text')
+//    end bot text div    //.,mkjuy6t5 4
+   
 // 4* //
     // const submitText = document.querySelector('submit')
     // submitText.innerText = input.value;
@@ -18,7 +53,10 @@ memeForm.addEventListener ('submit', function(e) {
     
 });
 
+
+
 // currently just for click targets
 bodyClick.addEventListener('click', function(e) {
     console.log(e.target)
 })
+
