eb27c43ea6c46d48abfd606c1a91eaf431f7c19d
DaveyPDev <74942934+DaveyPDev@users.noreply.github.com>
2022-02-08 21:39:35 -0500

    Submitting
    Almost have all the tasks done.
    
    Issue when submitting a new form creates texts fine. when submitting a second image, instead of each image having its own top/bot text the div gets on pair of top/bot text between both images.

--- script.js
+++ script.js
@@ -1,21 +1,81 @@
-// 3* //
-const form = document.querySelector('form');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
-    window.history.back()
-    
-// 4* //
-    const submitText = document.querySelector('submit')
-    submitText.innerText = input.value;
-    input.value = '';
-    
-}, true);
+const memeForm = document.getElementById('formID')
+const topText = document.getElementById('top-text')
+const botText = document.getElementById('bot-text')
+const memeUrl = document.getElementById('meme-img');
+const bodyClick = document.querySelector('body')
 
-
-form.addEventListener('click', function(e) {
-    e.preventDefault()
-    
-   
+bodyClick.addEventListener('click', function(e) {
     console.log(e)
+
+    if( e.target.tagName === 'BUTTON') {
+        e.target.parentElement.remove();
+    }
 })
+
+memeForm.addEventListener ('submit', function(e) {
+    // 3* //
+    e.preventDefault()
+//    start top text   //
+//   end top text    //
+//    start bot text    //
+//    end bot text   //
+//    start url submit
+   
+// 5*  //
+//   start top text div   //
+  
+//    end bot text div    //
+   
+// 4* //
+    // const submitText = document.querySelector('submit')
+    // submitText.innerText = input.value;
+    // input.value = '';
+    
+    memeGen(memeUrl.value, topText.value, botText.value);
+    memeUrl.value = '';
+    topText.value = '';
+    botText.value = '';
+});
+
+function memeGen (memeUrl, topText, botText) {
+
+//    start img div    //
+const newDivImg = document.createElement('div')
+const parentContainerImg = document.querySelector('.container')
+const memeImg = document.createElement('img')
+parentContainerImg.append(newDivImg)
+
+memeImg.classList.add('meme-img')
+newDivImg.append(memeImg)
+memeImg.setAttribute('src', memeUrl)
+memeUrl = ''
+//    end img div    //
+
+//   start top text div   //
+const newDivTop = document.createElement('div')
+const parentContainerTop = document.querySelector('.container')
+parentContainerTop.appendChild(newDivTop)
+newDivTop.innerText = topText;
+newDivTop.classList.add('top-text')
+// newDivTop.setAttribute('p', topText)
+topText = ''
+//    end top text div    //
+
+//    start bot text div    //
+const newDivBot = document.createElement('div')
+const parentContainerBot = document.querySelector('.container')
+parentContainerBot.appendChild(newDivBot)
+newDivBot.innerText = botText;
+newDivBot.classList.add('bot-text')
+// newDivBot.setAttribute('p', botText)
+botText = ''
+//    end bot text div    //
+
+//    start remove button    //
+const removeBtn = document.createElement('button')
+removeBtn.innerText = 'X'
+newDivImg.appendChild(removeBtn);
+removeBtn.classList.add('removeBtn')
+//    end remove button    //
+};
