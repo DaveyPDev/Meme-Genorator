9706a64a28b88190700d66680244b280e0c66f04
DaveyPDev <74942934+DaveyPDev@users.noreply.github.com>
2022-02-13 22:50:59 -0500

    Update script.js
    cleaned up my OG submit

--- script.js
+++ script.js
@@ -1,37 +1,13 @@
-
 const memeForm = document.getElementById('formID')
 const topText = document.getElementById('top-text')
 const botText = document.getElementById('bot-text')
 const memeUrl = document.getElementById('meme-img');
-const bodyClick = document.querySelector('body')
+const mainDiv = document.getElementById('.container')
 
-bodyClick.addEventListener('click', function(e) {
-    console.log(e)
-
-    if( e.target.tagName === 'BUTTON') {
-        e.target.parentElement.remove();
-    }
-})
-
-memeForm.addEventListener ('submit', function(e) {
+    memeForm.addEventListener ('submit', function(e) {
     // 3* //
     e.preventDefault()
-//    start top text   //
-//   end top text    //
-//    start bot text    //
-//    end bot text   //
-//    start url submit
-   
-// 5*  //
-//   start top text div   //
-  
-//    end bot text div    //
-   
-// 4* //
-    // const submitText = document.querySelector('submit')
-    // submitText.innerText = input.value;
-    // input.value = '';
-    
+
     memeGen(memeUrl.value, topText.value, botText.value);
     memeUrl.value = '';
     topText.value = '';
@@ -41,41 +17,44 @@ memeForm.addEventListener ('submit', function(e) {
 function memeGen (memeUrl, topText, botText) {
 
 //    start img div    //
-const newDivImg = document.createElement('div')
-const parentContainerImg = document.querySelector('.container')
-const memeImg = document.createElement('img')
-parentContainerImg.append(newDivImg)
+    const newDivImg = document.createElement('div')
+    const parentContainerImg = document.querySelector('.container')
+    const memeImg = document.createElement('img')
+    parentContainerImg.append(newDivImg)
 
-memeImg.classList.add('meme-img')
-newDivImg.append(memeImg)
-memeImg.setAttribute('src', memeUrl)
-memeUrl = ''
+    memeImg.classList.add('meme-img')
+    newDivImg.append(memeImg)
+    memeImg.setAttribute('src', memeUrl)
 //    end img div    //
 
 //   start top text div   //
-const newDivTop = document.createElement('div')
-const parentContainerTop = document.querySelector('.container')
-parentContainerTop.appendChild(newDivTop)
-newDivTop.innerText = topText;
-newDivTop.classList.add('top-text')
-// newDivTop.setAttribute('p', topText)
-topText = ''
+    const newDivTop = document.createElement('p')
+    const parentContainerTop = document.querySelector('.container')
+    parentContainerTop.append(newDivTop)
+    newDivTop.innerText = topText;
+    newDivTop.classList.add('top-text')
 //    end top text div    //
 
 //    start bot text div    //
-const newDivBot = document.createElement('div')
-const parentContainerBot = document.querySelector('.container')
-parentContainerBot.appendChild(newDivBot)
-newDivBot.innerText = botText;
-newDivBot.classList.add('bot-text')
-// newDivBot.setAttribute('p', botText)
-botText = ''
+    const newDivBot = document.createElement('p')
+    const parentContainerBot = document.querySelector('.container')
+    parentContainerBot.append(newDivBot)
+    newDivBot.innerText = botText;
+    newDivBot.classList.add('bot-text')
 //    end bot text div    //
 
 //    start remove button    //
-const removeBtn = document.createElement('button')
-removeBtn.innerText = 'X'
-newDivImg.appendChild(removeBtn);
-removeBtn.classList.add('removeBtn')
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = 'X'
+    newDivImg.appendChild(removeBtn);
+    removeBtn.classList.add('removeBtn')
 //    end remove button    //
+
+mainDiv.addEventListener('click', function(e) {
+  
+    if( e.target.tagName === 'BUTTON') {
+        e.target.parentElement.remove();
+    }
+})
+
 };
